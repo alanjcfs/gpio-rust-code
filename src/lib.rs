1f710d2a46f69f9333e5d6abbb9a3a6f3e29b53e
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2016-10-22 16:14:21 -0700

    Hit a wall with opening up an empty file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+extern crate libc;
+extern crate memmap;
+use std::fs::File;
+use std::io;
+use memmap::{Mmap, Protection};
+
 // const BCM2708_PERI_BASE_DEFAULT: usize = 0x20000000;
 // const BCM2709_PERI_BASE_DEFAULT: usize = 0x3f000000;
 // const GPIO_BASE_OFFSET: usize = 0x200000;
@@ -14,22 +20,27 @@
 // const PULLUPDNCLK_OFFSET: usize = 38;
 
 
-// const SETUP_OK: usize =           0
-// const SETUP_DEVMEM_FAIL: usize =  1
-// const SETUP_MALLOC_FAIL: usize =  2
-// const SETUP_MMAP_FAIL: usize =    3
-// const SETUP_CPUINFO_FAIL: usize = 4
-// const SETUP_NOT_RPI_FAIL: usize = 5
-//
-// const INPUT: usize =  1 // is really 0 for control register!
-// const OUTPUT: usize = 0 // is really 1 for control register!
-// const ALT0: usize =   4
-//
-// const HIGH: usize = 1
-// const LOW: usize =  0
-//
-// const PUD_OFF: usize =  0
-// const PUD_DOWN: usize = 1
-// const PUD_UP: usize =   2
+const SETUP_OK: usize =           0;
+// const SETUP_DEVMEM_FAIL: usize =  1;
+// const SETUP_MALLOC_FAIL: usize =  2;
+const SETUP_MMAP_FAIL: usize =    3;
+// const SETUP_CPUINFO_FAIL: usize = 4;
+// const SETUP_NOT_RPI_FAIL: usize = 5;
+// const INPUT: usize =  1;// is really 0 for control register!
+// const OUTPUT: usize = 0;// is really 1 for control register!
+// const ALT0: usize =   4;
+// const HIGH: usize = 1;
+// const LOW: usize =  0;
+// const PUD_OFF: usize =  0;
+// const PUD_DOWN: usize = 1;
+// const PUD_UP: usize =   2;
 
-
+pub fn setup() -> Result<Mmap, io::Error> {
+    if let Ok(mem_fd) = File::open("/dev/gpiomem") {
+        println!("{:?}",mem_fd.metadata().unwrap().file_type());
+    // gpio_map = (uint32_t *)mmap(NULL, BLOCK_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, mem_fd, 0);
+        Mmap::open(&mem_fd, Protection::ReadWrite)
+    } else {
+        panic!("Could not query /dev/gpiomem")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
-use std::fs::File;
-use std::io::*;
-
 extern crate gpio_rust_code;
 
 fn main() {
-  let mut f = open_file("/dev/gpiomem").unwrap();
-  let mut buf = Vec::new();
-
-  let result = f.read_to_end(&mut buf);
-
-  println!("{:?}", result);
+    let foo = gpio_rust_code::setup();
+    println!("{:?}", foo)
 }
 
-fn open_file(dev: &str) -> std::result::Result<File, Error> {
-  File::open(dev)
-}
-
-/* Because escalated privileges are needed to access /dev/gpiomem, we need to
- * mock the device */
 #[test]
 fn it_opens() {
-  assert_eq!(open_file("/dev/gpiomem").is_ok(), false);
 }
